5cd93740f0f11613415e77b490e53d8e658fb0a8
Joel Waymack <joel.waymack@microsoft.com>
2025-04-14 12:30:54 -0500

    Remove unused config value

--- Graph.Api/DataAccess/PostgresConfig.cs
+++ Graph.Api/DataAccess/PostgresConfig.cs
@@ -1,6 +1,6 @@
 namespace Graph.Api.DataAccess;
 
-public record PostgresConfig(string ConnectionString, string DatabaseName, string GraphName)
+public record PostgresConfig(string ConnectionString, string GraphName)
 {
     public static string ConfigSectionName => "Postgres";
 }
