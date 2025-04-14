a7b7ec94411c233f7cd87a5b60cc01627cb71422
Joel Waymack <joel.waymack@microsoft.com>
2025-04-14 12:30:07 -0500

    Remove vertex base

--- Graph.Api/Models/Asset.cs
+++ Graph.Api/Models/Asset.cs
@@ -1,9 +1,8 @@
-using Npgsql.Age.Types;
-
 namespace Graph.Api.Models;
 
-public class Asset : VertexBase
+public class Asset
 {
+    public string Id { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
     public string Description { get; set; }
--- Graph.Api/Models/VertexBase.cs
+++
@@ -1,6 +0,0 @@
-namespace Graph.Api.Models;
-
-public class VertexBase
-{
-    public string Id { get; set; }
-}
